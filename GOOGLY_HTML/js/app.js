a24320e72461ed156b26408e75508fcf0bc4c6cc
Ross Nover <ross@friendlydesign.co>
2015-05-16 18:11:09 -0400

    Layout updates.

--- GOOGLY_HTML/js/app.js
+++ GOOGLY_HTML/js/app.js
@@ -1,14 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-var masonryGo = function() {
-    var container = document.querySelector('#container');
-    var msnry = new Masonry( container, {
-      // options
-      columnWidth: 400,
-      itemSelector: '.item'
-    });
-};
+// var masonryGo = function() {
+//     var container = document.querySelector('#container');
+//     var msnry = new Masonry( container, {
+//       // options
+//       columnWidth: 400,
+//       itemSelector: '.item'
+//     });
+// };
 // =======
 
 // var container = document.querySelector('#container');
