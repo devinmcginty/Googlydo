8161337da4469c0007ca867bf932b9807cbbf71b
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-05-16 17:34:24 -0400

    Don't know

--- GOOGLY_HTML/js/app.js
+++ GOOGLY_HTML/js/app.js
@@ -1,9 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+<<<<<<< HEAD
 $(document).on('ready page:load', function () {
     $(document).foundation();
 });
 
+=======
+$(document).foundation();
+<<<<<<< Updated upstream
+>>>>>>> 97064ab8ecb0e23ed993ff2e56225b64f648ab90
 var masonryGo = function() {
     var container = document.querySelector('#container');
     var msnry = new Masonry( container, {
@@ -12,3 +17,12 @@ var masonryGo = function() {
       itemSelector: '.item'
     });
 };
+=======
+
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 400,
+  itemSelector: '.item'
+});
+>>>>>>> Stashed changes
