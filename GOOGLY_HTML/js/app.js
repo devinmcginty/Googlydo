da810211647f4d880bd2675b17629e1b9c731fb7
Ross Nover <ross@friendlydesign.co>
2015-05-16 17:29:20 -0400

    Removed Raleway.

--- GOOGLY_HTML/js/app.js
+++ GOOGLY_HTML/js/app.js
@@ -1,6 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+<<<<<<< Updated upstream
 var masonryGo = function() {
     var container = document.querySelector('#container');
     var msnry = new Masonry( container, {
@@ -9,3 +10,12 @@ var masonryGo = function() {
       itemSelector: '.item'
     });
 };
+=======
+
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 400,
+  itemSelector: '.item'
+});
+>>>>>>> Stashed changes
